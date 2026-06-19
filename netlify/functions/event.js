0e2b0fd73de7faf1c6404fe142020b709c03ed45
alexek5 <alsa.0705@gmail.com>
2026-06-19 23:46:14 +0200

    add event function

--- netlify/functions/event.js
+++ netlify/functions/event.js
@@ -2,16 +2,19 @@ let latestEvent = null;
 
 export async function handler(event) {
 
+  // POST = spara event
   if (event.httpMethod === "POST") {
-
     latestEvent = JSON.parse(event.body);
 
     return {
       statusCode: 200,
-      body: "OK"
+      body: JSON.stringify({
+        ok: true
+      })
     };
   }
 
+  // GET = hämta senaste event
   return {
     statusCode: 200,
     body: JSON.stringify(latestEvent)
